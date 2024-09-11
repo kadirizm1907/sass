d3bd745ee868f0a00e8943a27d38b90556c95ba9
kadirizm <kadiryurt1907@gmail.com>
2024-09-11 11:28:36 +0200

    last styling completed

--- ReactSassProductList/src/components/products/ProductCard.jsx
+++ ReactSassProductList/src/components/products/ProductCard.jsx
@@ -24,7 +24,13 @@ const ProductCard = ({ title, image, price }) => {
       <div className="price">
         <h3>{price} $</h3>
       </div>
-      <img src={image} alt={title} />
+      <div className="con-image" style={{
+            backgroundImage: `url(${image})`,
+            backgroundSize: "80%",     
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat"}} >
+       {/* <img src={image} alt={title} />  */}
+      </div>
       <div className="card__over">
         <h2>{title}</h2>
       </div>
